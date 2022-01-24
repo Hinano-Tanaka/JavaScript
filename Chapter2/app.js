c10425bcb70f8bedc3b3eb35c4a3fb4bb63dea4c
Hinano-Tanaka <95991554+Hinano-Tanaka@users.noreply.github.com>
2022-01-24 09:18:33 +0900

    [Finish] Chapter1~2

---
+++ Chapter2/app.js
@@ -0,0 +1,56 @@
+// let hello = 'Hello World';
+// alert(hello);
+
+// 整数を代入する
+let int1 = 1;
+
+// 負数を代入する
+let int2 = -10;
+
+// 小数点を代入する
+let float1 = 3.14;
+
+// 文字列を代入する
+let str1 = 'JavaScriptを覚えよう';
+
+let str1 = 'Hello';
+let str2 = 'World!!';
+alert(str1 + str2);
+
+
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い');
+}
+
+let max = 100;
+let num = 1;
+let count = 0;
+
+while(num < max){
+  num = num * 2;
+  count = count + 1;
+}
+
+alert('2を掛け続けて' + max + 'を超えるのに必要だった回数は' + count + '回です');
+
+// -----------------------------------
+do{
+  最低1回は行われる処理
+}while(条件式);
+
+// ------------------------------------
+let i;
+let num = 0;
+
+for(i = 1; i < 11; i++){
+  num = num + i;
+}
+
+alert('1から10まで足し算した結果は' + num + 'です');
